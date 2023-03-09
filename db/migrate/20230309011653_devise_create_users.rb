10e7ac2063c682af167b4d45c400bdfb08dde136
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-08 22:24:27 -0300

    adding provider and uid

--- db/migrate/20230309011653_devise_create_users.rb
+++ db/migrate/20230309011653_devise_create_users.rb
@@ -14,6 +14,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Rememberable
       t.datetime :remember_created_at
 
+      t.string  :provider
+      t.string :uid
+
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
